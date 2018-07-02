f212e89d113d59dce99a05e88792f829dd6df6cc
nadaveshed <nadav3200@gmail.com>
2018-07-02 16:53:01 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit } from '@angular/core';
+import { ApiService } from "./api.service"
+import { MyNewInterface } from "./my-new-interface";
+import { error } from 'util';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ApiService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'book library';
+
+  _postsArray: MyNewInterface[];
+
+  constructor(private apiSerivce: ApiService){}
+
+  getPosts(): void {
+    this.apiSerivce.getPosts().
+    subscribe(
+       resultArray => this._postsArray =
+       resultArray,
+       error => console.log("Error :: " + error ))
+  }
+  ngOnInit(): void{
+    this.getPosts();
+
+  }
 }
---
+++ src/app/my-new-interface.ts
@@ -0,0 +1,6 @@
+export interface MyNewInterface {
+    userId: number;
+    id:number;
+    title:string;
+    body:string;
+}
