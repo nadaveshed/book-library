8495bfdca5e7ad2b3d32ee8f14e61c11188419f0
nadaveshed <nadav3200@gmail.com>
2018-07-17 06:37:43 +0300

    first

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files frim the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname+ '/dist/book-library/index.html'));
+app.use(express.static(__dirname+ '/dist/index.html'));
 
 app.get('*', function(req,res) {
 
-    res.sendfile(path.join(__dirname+ '/dist/book-library/index.html'));
+    res.sendfile(path.join(__dirname+ '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8000);
